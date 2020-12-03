8b4c5ae5aaef7fa7cc2347304c6bcf4c4163e8ab
lisa_schmitz <lisa.schmitz66@gmail.com>
2020-12-03 15:46:03 +0100

    Solution day 3

--- day3/day3.rb
+++ day3/day3.rb
@@ -1,13 +1,24 @@
 data = File.readlines('input_day3.txt')
-
 line_length = data[0].delete("\n").length
 
-i = 0
-tree_count = 0
+slopes = {
+  slope_r_1_d_1: { r: 1, d: 1, tree_count: 0 },
+  slope_r_3_d_1: { r: 3, d: 1, tree_count: 0 },
+  slope_r_5_d_1: { r: 5, d: 1, tree_count: 0 },
+  slope_r_7_d_1: { r: 7, d: 1, tree_count: 0 },
+  slope_r_1_d_2: { r: 1, d: 2, tree_count: 0 },
+}
 
-data.each do |d|
-  tree_count += 1 if d[i % line_length] == '#'
-  i = i + 3
+data.each_with_index do |d, index|
+  slopes.each do |key, value|
+    next if index % value[:d] > 0
+    r = value[:r] * (index / value[:d])
+    value[:tree_count] = value[:tree_count] + 1 if d[r % line_length] == '#'
+  end
 end
 
-puts "# tree count: #{tree_count}"
+puts "# tree count"
+puts slopes
+
+tree_product = slopes.values.map{|e| e[:tree_count]}.reduce(:*)
+puts "# tree product: #{tree_product}"
